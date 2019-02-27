34d6bdcdb698558ad52f716dd405d07183d1f2ef
Du Nguyen <nguyend33@gmail.com>
2019-02-26 20:56:26 -0700

    add validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,33 @@
 class Person < ApplicationRecord
-end
+  # Assoc
+  # Validations
+  validates :age, numericality: { 
+    less_than_or_equal_to: 150,
+    greater_than_or_equal_to: 18
+  }
+
+  validates :alive, inclusion: {
+    in: %w (true, false),
+    message: "%{value} is invalid"
+  }
+
+  validates :gender, :name, :age, :alive, presence: true
+
+  validates :hair_color, :eye_color, allow_blank: true
+
+  validates: :name, uniqueness: true
+  # Methods
+  # Class method
+    # callback
+    def self.order_age
+      order(:age)
+    end
+    
+  # Instance method
+    before_create :set_alive
+  
+    def set_alive
+      self.alive = true
+    end
+  end
+  
